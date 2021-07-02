6ee54bd0873fb7bf4b975b1fcd60bd3706cc27ff
Junaid21-source <76106953+Junaid21-source@users.noreply.github.com>
2021-07-02 16:17:12 +0600

    Add files via upload
    Follow the comments in the code

---
+++ main.py
@@ -0,0 +1,10 @@
+# first try to confirm if there is or not, the letter you are trying to find
+# write the text in the quoted box from which you are trying to find the letter
+Indicator = ""
+# write the letter you are trying to find in the quoted box
+print("" in Indicator)
+# paste the text
+test = ""
+# write the letter you are trying to find in the quoted box
+print(test.find(""))
+# then run it
